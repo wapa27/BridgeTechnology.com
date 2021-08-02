05e833f2a39b182c38736046576a6984ff71c06b
wapa1274 <wapa1274@colorado.edu>
2021-08-01 22:09:38 -0700

    Initial Spring setup complete, will need additional dependencies

---
+++ bridgetechnology/src/main/java/com/bridgetechnology/bridgetechnology/BridgetechnologyApplication.java
@@ -0,0 +1,16 @@
+package com.bridgetechnology.bridgetechnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgetechnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BridgetechnologyApplication.class, args);
+		System.out.println("working fine");
+		int x = 0;
+		x++;
+	}
+
+}
---
+++ bridgetechnology/src/test/java/com/bridgetechnology/bridgetechnology/BridgetechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgetechnology.bridgetechnology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgetechnologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
