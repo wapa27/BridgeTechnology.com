bb211bc8428c2185c62ee665cad6fc21397c6dbe
wapa1274 <wapa1274@colorado.edu>
2021-11-21 18:17:42 -0700

    Added new page About Bridge; cleaned up; get images from godaddy

--- bridgetechnology/frontend/src/app/pages/about-bridge-tech/about-bridge-tech.component.spec.ts
+++ bridgetechnology/frontend/src/app/pages/about-bridge-tech/about-bridge-tech.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { AboutBridgeTechComponent } from './about-bridge-tech.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('AboutBridgeTechComponent', () => {
+  let component: AboutBridgeTechComponent;
+  let fixture: ComponentFixture<AboutBridgeTechComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ AboutBridgeTechComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(AboutBridgeTechComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
